636a259434fc88139bcd25498de07dab2fd1c7d4
parsa011 <parsa32569@gmail.com>
2023-02-06 23:34:43 +0330

    detect control keys

--- src/basic.h
+++ src/basic.h
@@ -1,6 +1,8 @@
 #ifndef _BASIC_H
 # define _BASIC_H
 
+#define CTRL_KEY(c) (c - 96)
+
 #define public extern
 #define private static
 
--- src/main.c
+++ src/main.c
@@ -1,26 +1,20 @@
 #include <stdio.h>
 #include <unistd.h>
 #include "tty.h"
+#include <ctype.h>
 
 int main()
 {
 	tty_raw_mode();
 	char c = tty_get_char();
 	while (c) {
-		printf("%d\n", c);
-		if (c == 'q') {
+		printf("%c\n", c);
+		if (CTRL_KEY('q') == c) {
 			printf("we got a 'q'\n");
 			break;
 		}
 		c = tty_get_char();
 	}
-	/* char c; */
-	/* while (read(STDIN_FILENO, &c, 1)) { */
-	/* 	if (c == 'q') */
-	/* 		break; */
-	/* 	printf("%d\n\b", c); */
-	/* } */
-	/* printf("Hello World!\n"); */
 	tty_cooked_mode();
 	return 0;
 }
