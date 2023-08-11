5ac97857b0eb306e744e555eaa7d89e6f7c8c7ef
parsa011 <parsa32569@gmail.com>
2023-08-11 23:53:26 +0330

    [tty.h] : Fix terminal prev char function name

--- src/tty.h
+++ src/tty.h
@@ -1,8 +1,14 @@
 #ifndef _TTY_H
 # define _TTY_H
 
-#include "basic.h"
 #include <termios.h>
+#include "basic.h"
+
+/* 
+ * 	write a character to the output buffer.  If it becomes
+ * 	full (obufp == OBUFSIZE), call tty_flush()
+ */
+#define OBUFSIZE 512
 
 public void tty_raw_mode();
 
@@ -13,6 +19,24 @@ public char tty_get_char(int *pending);
 public int tty_window_size(int *rows, int *cols);
 
 public int tty_cursor_pos_get(int *rows, int *cols);
+
+/*
+ *	Write given string into out put buffer using ttyputc()
+ *	sometimes we need to write output immediatly , so we will pass
+ *	'true' for flush_now to autocall for tty_flush()
+ */
+public void tty_put_string(char *s, bool flush_now);
+
+/*
+ *	add given given into out put array
+ *	call tty_flush() when we reached to OBUFSIZE size
+ */
+public void tty_put_char(char c);
+
+/*
+ *	Print output to terminal by calling write()
+ */
+public void tty_flush();
 
 public void tty_clear();
 
@@ -30,6 +54,6 @@ public void tty_cursor_line_prev();
 
 public void tty_cursor_char_next();
 
-public void tty_cursor_prev_char();
+public void tty_cursor_char_prev();
 
 #endif
