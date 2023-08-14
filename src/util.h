8545a48d40c37f3a8aad4a6e7586e7a95177242d
parsa011 <parsa32569@gmail.com>
2023-08-15 01:15:29 +0330

    [util.c] : utility functions like string format and etc

---
+++ src/util.h
@@ -0,0 +1,10 @@
+#ifndef _UTIL_H
+# define _UTIL_H
+
+/*
+ *	make string with given format and returns it pointer
+ *	also this code is from printf manual page :)
+ */
+char *make_fmt_string(const char *fmt, ...);
+
+#endif
