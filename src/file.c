05d2ea8fbe69c4bbe68f374762e3587e2310f4a3
parsa011 <parsa32569@gmail.com>
2023-08-15 01:28:42 +0330

    [file.h/c] : new files for file utility function

---
+++ src/file.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+#include "file.h"
+
+char* file_name(char* path)
+{
+	size_t i = 0;
+	size_t pos = 0;
+	char* p = (char*)path;
+	while (*p) {
+		i++;
+		if (*p == '/' || *p == '\\') {
+			pos = i;
+		}
+		p++;
+	}
+	return path + pos;
+}
---
+++ src/file.h
@@ -0,0 +1,12 @@
+#ifndef _FILE_H
+# define _FILE_H
+
+/*
+ * returns pointer to start of file name in givn path
+ * for example :
+ * 		path : /src/main.c
+ * points to here   ^ 
+ */
+char *file_name(char *path);
+
+#endif
