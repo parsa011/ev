90e159640d352973c4fe01ab76e5d05d0e64a9b1
parsa011 <parsa32569@gmail.com>
2023-08-18 02:19:50 +0330

    improve next line command

--- src/commands/j.c
+++ src/commands/j.c
@@ -1,7 +1,20 @@
+#include <stddef.h>
 #include "j.h"
+#include "../editor.h"
 
 public return_message j_command(char **args)
 {
-	tty_cursor_line_next();
-	return create_return_message(true, "Next Line");
+	editor_buffer_t *buf = editor_buffer();
+	/*
+	 * if buffer row is less than editor row count, go to next line
+	 * otherwise scroll one line to down (if there is any more lines)
+	 */
+	if (L_LINK_NEXT(buf->current_line) == NULL)
+		return create_return_message(ERROR, "end of buffer");
+	if (buf->pos.row < editor.rows) {
+		buf->pos.row++;
+		buf->current_line = L_LINK_NEXT(buf->current_line);
+		tty_cursor_line_next();
+	}
+	return create_return_message(SUCCESS, "Next Line");
 }
