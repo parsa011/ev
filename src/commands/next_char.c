eccba6a6104114b307921996a41f96f6a10f0438
parsa011 <parsa32569@gmail.com>
2023-08-19 20:54:09 +0330

    go to next or prev line when moving in chars
    if we are in beginning of line, and we presed prev_char
    we will go back to prev line, also same thing for next_char
    but will go to next_line

--- src/commands/next_char.c
+++ src/commands/next_char.c
@@ -1,5 +1,7 @@
 #include <stddef.h>
 #include "next_char.h"
+#include "next_line.h"
+#include "beginning_of_line.h"
 #include "../editor.h"
 
 public return_message next_char_command(char **args)
@@ -10,8 +12,10 @@ public return_message next_char_command(char **args)
 	 * if buffer row is less than editor row count, go to next line
 	 * otherwise scroll one line to down (if there is any more lines)
 	 */
-	if (buf->char_offset + 1 >= cl->len)
-		return create_return_message(ERROR, "end of line");
+	if (buf->char_offset + 1 >= cl->len) {
+		next_line_command(NULL);
+		return beginning_of_line_command(NULL);
+	}
 	if (*(buf->current_line->str + buf->char_offset) == '\t')
 		buf->pos.col += TAB_SIZE;
 	else
--- src/commands/prev_char.c
+++ src/commands/prev_char.c
@@ -1,12 +1,19 @@
 #include <stddef.h>
 #include "next_char.h"
+#include "prev_line.h"
+#include "end_of_line.h"
 #include "../editor.h"
 
 public return_message prev_char_command(char **args)
 {
 	editor_buffer_t *buf = editor_buffer();
-	if (buf->char_offset == 0)
-		return create_return_message(ERROR, "beginning of line");
+	if (buf->char_offset == 0) {
+		if (L_LINK_PREV(buf->current_line)) {
+			prev_line_command(NULL);
+			return end_of_line_command(NULL);
+		}
+		return create_return_message(ERROR, "no any line");
+	}
 	buf->char_offset--;
 	if (*(buf->current_line->str + buf->char_offset) == '\t')
 		buf->pos.col -= TAB_SIZE;
