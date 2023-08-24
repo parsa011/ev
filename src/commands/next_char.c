59bb1752e5757da6a28a162476edfd700b2f7423
parsa011 <parsa32569@gmail.com>
2023-08-24 17:25:39 +0330

    prevent of going to bol when there is no any more line

--- src/commands/next_char.c
+++ src/commands/next_char.c
@@ -9,8 +9,10 @@ public return_message next_char_command(char **args)
 	buffer_t *buf = editor_buffer();
 	line_t *cl = buf->current_line;
 	if (buf->char_offset + 1 > cl->len) {
-		next_line_command(NULL);
-		return beginning_of_line_command(NULL);
+		if (next_line_command(NULL).status == SUCCESS)
+			return beginning_of_line_command(NULL);
+		else
+			return create_return_message(ERROR, "end of buffer");
 	}
 	if (*(buf->current_line->str + buf->char_offset) == '\t')
 		buf->pos.col += TAB_SIZE;
