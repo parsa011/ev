221dc54958bd6d2b50c7fac904091652bd01adfe
parsa011 <parsa32569@gmail.com>
2023-08-25 20:44:28 +0330

    append next to current line if cursor in end in kill_line_command()

--- src/commands/kill_line.c
+++ src/commands/kill_line.c
@@ -1,15 +1,21 @@
+#include <stddef.h>
 #include "kill_line.h"
+#include "next_char.h"
+#include "../buffer.h"
+#include "../key.h"
 
 public return_message kill_line_command(char **args)
 {
 	buffer_t *buf = editor_buffer();
 	line_t *current_line = buf->current_line;
 
-	bool is_end = buf->char_offset + 1 == current_line->len;
-	line_delete_range(current_line, buf->char_offset, current_line->len);
+	bool is_end = buf->char_offset == current_line->len;
 	if (is_end) {
+		if (next_char_command(NULL).status == SUCCESS)
+			buffer_insert_key(BACKSPACE);
+	} else {
+		line_delete_range(current_line, buf->char_offset, current_line->len);
 	}
-		// TODO : append next line to current line and remove it
 	buf->render = true;
 	return create_return_message(SUCCESS, "kill line");
 }
