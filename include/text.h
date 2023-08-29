26a36990593270eaa7ebdd5548e1217caedc067a
parsa011 <parsa32569@gmail.com>
2023-08-30 02:54:28 +0330

    remove redundant files and add tasks to TODO

--- include/text.h
+++
@@ -1,6 +0,0 @@
-#ifndef _TEXT_H
-# define _TEXT_H
-
-#include "basic.h"
-#include "editor.h"
-#endif
--- src/text.c
+++
@@ -1,7 +0,0 @@
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include "text.h"
-#include "log.h"
-
-
