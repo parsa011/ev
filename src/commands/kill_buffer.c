c06ffe551df33dbb826bcf719e85a6fd142ee935
parsa011 <parsa32569@gmail.com>
2023-09-18 01:24:04 +0330

    code refactor for if statement in kill_buffer.c

--- src/commands/kill_buffer.c
+++ src/commands/kill_buffer.c
@@ -5,10 +5,7 @@
 public return_message kill_buffer_command(char **args)
 {
 	buffer_t *buf = editor_buffer();
-	buffer_t *new_buf = L_LINK_NEXT(buf);
-	if (!new_buf) {
-		new_buf = L_LINK_PREV(buf);
-	}
+	buffer_t *new_buf = !L_LINK_NEXT(buf) ? L_LINK_PREV(buf) : L_LINK_NEXT(buf);
 	if (!new_buf) {
 		editor.exit = true;
 		return create_return_message(SUCCESS, "last buffer, exiting");
